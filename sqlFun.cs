f36fcb765a6684a97ca6d7b6d3fcf882147203c4
smile <2230550672@qq.com>
2020-06-03 09:07:13 +0800

    add 数据库操作文件

---
+++ sqlFun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace music
+{
+    class sqlFun
+    {
+        public static string GetConnectionString()
+        {
+            string strServer = AppDomain.CurrentDomain.BaseDirectory + "SqlConfig.txt";
+            return File.ReadAllText(strServer);
+        }
+    }
+}
